1c058bc4990cd275b0536671655e05ba8419a25b
Mason Brignac <masonbrignac@Masons-MacBook-Pro.local>
2023-05-08 16:25:35 -0500

    sytnax correction before deploy

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,7 +1,8 @@
 import { signOut } from 'next-auth/react';
 import React from 'react';
 
-import useCurrentUser from '../hooks/useCurrentUser';
+import useCurrentUser from '@/hooks/useCurrentUser';
+
 
 interface AccountMenuProps {
   visible?: boolean;
