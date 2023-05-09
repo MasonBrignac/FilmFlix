b219c3fb8232474188bffd0eb5f3b69fac153619
Mason Brignac <masonbrignac@Masons-MacBook-Pro.local>
2023-05-09 02:02:10 -0500

    bugs

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -2,16 +2,27 @@ import axios from 'axios';
 import React, { useCallback, useMemo } from 'react';
 import { AiOutlinePlus, AiOutlineCheck } from 'react-icons/ai';
 import { useSession } from 'next-auth/react';
+import type { Session } from 'next-auth';
 
 import useCurrentUser from '@/hooks/useCurrentUser';
 import useFavorites from '@/hooks/useFavorites';
 
 interface FavoriteButtonProps {
-  movieId: string
+  movieId: string;
+}
+
+function hasSession(obj: any): obj is { session: Session | null; status: string } {
+  return 'session' in obj;
 }
 
 const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
-  const { session, status } = useSession();
+  const sessionData = useSession();
+
+  if (!hasSession(sessionData)) {
+    return null;
+  }
+
+  const { session, status } = sessionData;
   console.log('Session in FavoriteButton:', session);
 
   const { mutate: mutateFavorites } = useFavorites();
@@ -19,10 +30,9 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
 
   const isFavorite = useMemo<boolean>(() => {
     const list = currentUser?.favoriteIds || [];
-  
+
     return list.includes(movieId);
   }, [currentUser, movieId]);
-  
 
   const toggleFavorites = useCallback(async () => {
     let response;
@@ -35,19 +45,19 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
 
     const updatedFavoriteIds = response?.data?.favoriteIds;
 
-    mutate({ 
-      ...currentUser, 
+    mutate({
+      ...currentUser,
       favoriteIds: updatedFavoriteIds,
     });
     mutateFavorites();
   }, [movieId, isFavorite, currentUser, mutate, mutateFavorites]);
-  
+
   const Icon = isFavorite ? AiOutlineCheck : AiOutlinePlus;
 
   return (
-    <div 
+    <div
       onClick={toggleFavorites}
-    className="
+      className="
     cursor-pointer 
     group/item 
     w-6 
@@ -62,10 +72,11 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
     items-center 
     transition 
     hover:border-neutral-300
-    ">
+    "
+    >
       <Icon className="text-white" size={25} />
     </div>
-  )
-}
+  );
+};
 
 export default FavoriteButton;
